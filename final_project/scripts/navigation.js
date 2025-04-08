6862503892c53ee66299c9bd62cbd43272c2e6ed
Carloalalan08 <caloycena08@gmail.com>
2025-04-08 18:25:22 +0800

    Saved

--- final_project/scripts/navigation.js
+++ final_project/scripts/navigation.js
@@ -1,4 +1,4 @@
 // navigation.js
-document.querySelector(".hamburger").addEventListener("click", function() {
+document.querySelector(".hamburger").addEventListener("click", function () {
     document.querySelector("nav ul").classList.toggle("show");
 });
